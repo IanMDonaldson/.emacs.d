601ad53c9b01af2bb8ea25be8e2b7a144d1c290a
IanMDonaldson <imdonaldson@ualr.edu>
2018-11-24 17:18:52 -0600

    Switched to Rtags for C++ Development
    Configured ~/.emacs.d/init.el to reflect changes relating to the usage of Rtags. Included in init.el are usages of the packages rtags, company-rtags, flycheck-rtags, which-key, rainbow delimiters. Configurations of Rtags keybindings are also included, only the basics though. Chose to use flycheck-rtags instead of rtags' default syntax checker.

---
+++ elpa/company-rtags-20180730.338/company-rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-rtags" "20180730.338" "RTags back-end for company" '((emacs "24.3") (company "0.8.1") (rtags "2.10")) :commit "29d3ee6ec1d8c9c1895fcab0c9887a8f286c6ed6" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "http://rtags.net")
