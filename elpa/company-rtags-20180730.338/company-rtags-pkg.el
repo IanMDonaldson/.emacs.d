6bd6fb244e854fa20b0e670bbc90355ad9b031bc
IanMDonaldson <imdonaldson@ualr.edu>
2019-02-24 15:12:53 -0600

    laptop config, with better file structures

--- elpa/company-rtags-20180730.338/company-rtags-pkg.el
+++ elpa/company-rtags-20180730.338/company-rtags-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "company-rtags" "20180730.338" "RTags back-end for company" '((emacs "24.3") (company "0.8.1") (rtags "2.10")) :commit "29d3ee6ec1d8c9c1895fcab0c9887a8f286c6ed6" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "http://rtags.net")
+(define-package "company-rtags" "20180730.338" "RTags back-end for company" '((emacs "24.3") (company "0.8.1") (rtags "2.10")) :commit "f3c59dcb6fd066f2d5ce67ae350d38fe038e4049" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "http://rtags.net")
--- elpa/rtags-20180925.641/rtags-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rtags" "20180925.641" "A front-end for rtags" 'nil :commit "29d3ee6ec1d8c9c1895fcab0c9887a8f286c6ed6" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "http://rtags.net")
